97ea5985a2035e467fd2e9a01acf1732a3aa95e2
aspfeiff <dpfeiffer27@yahoo.com>
2017-10-29 02:38:04 -0600

    add

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name) VALUES ('Pulled Pork Burger');
+
+INSERT INTO burgers (burger_name) VALUES ('BBQ Bacon Burger');
+
+INSERT INTO burgers (burger_name) VALUES ('Double Cheeseburger');
+
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
